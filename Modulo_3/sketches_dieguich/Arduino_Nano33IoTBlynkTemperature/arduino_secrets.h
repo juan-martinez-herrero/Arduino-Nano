e5e7357598cd406aa37f66e4d09db98cfdfa3c50
juan-martinez-herrero <brontehecatonchires@hotmail.com>
2020-12-15 08:09:32 +0100

    docs(add documentation connect sensors to arduino nano): add documentation to connect sensors
    add documentation to connect sensors to Arduino Nano

---
+++ Modulo_3/sketches_dieguich/Arduino_Nano33IoTBlynkTemperature/arduino_secrets.h
@@ -0,0 +1,3 @@
+#define SECRET_SSID ""  //introducir tu nombre de wifi
+#define SECRET_PASS "" //introducir tu clave de la wifi de casa
+#define YOUR_TOKEN "" //introducir tu token personal (se puede enviar al correo desde la aplicación blynk o copiarlo y pegarlo desde Blynk)
